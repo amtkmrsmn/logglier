df9c7ed5b866d540ad6e9ed8ef0488bc95b8f3fd
Amit Kr. Suman <amtkmrsmn@gmail.com>
2015-08-19 18:26:08 +0530

    Update client_spec.rb

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -74,21 +74,24 @@ describe Logglier::Client do
     subject { Logglier::Client.new('https://localhost') }
 
     it "should mash out hashes" do
-      message = subject.massage_message({:foo => :bar},"WARN")
+      message = subject.massage_message({:foo => :bar},"WARN", 1023)
       message.should =~ /^severity=WARN,/
+      message.should =~ /^pid=1023,/
       message.should =~ /foo=bar/
     end
 
     it "should mash out nested hashes" do
-      message = subject.massage_message({:foo => :bar, :bazzle => { :bom => :bastic } }, "WARN")
+      message = subject.massage_message({:foo => :bar, :bazzle => { :bom => :bastic } }, "WARN", 1024)
       message.should =~ /^severity=WARN,/
+      message.should =~ /^pid=1024,/
       message.should =~ /foo=bar/
       message.should =~ /bazzle\.bom=bastic/
     end
 
     it "should mash out deeply nested hashes" do
-      message = subject.massage_message({:foo => :bar, :bazzle => { :bom => :bastic, :totally => { :freaking => :funny } } }, "WARN")
+      message = subject.massage_message({:foo => :bar, :bazzle => { :bom => :bastic, :totally => { :freaking => :funny } } }, "WARN", 1025)
       message.should =~ /^severity=WARN,/
+      message.should =~ /^pid=1026,/
       message.should =~ /foo=bar/
       message.should =~ /bazzle\.bom=bastic/
       message.should =~ /bazzle\.totally\.freaking=funny/
