4fac845aa6b9b057f0400ebe0a585b3e04448163
Amit Kr. Suman <amtkmrsmn@gmail.com>
2015-08-19 18:27:30 +0530

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,21 +38,22 @@ shared_examples_for "a logglier enhanced Logger instance" do
   context "#add" do
     context "with a string" do
       it "should send a message via the logdev" do
-        subject.logdev.dev.should_receive(:write).with(/severity=WARN, foo/)
-        subject.add(Logger::WARN) { 'foo' }
+        subject.logdev.dev.should_receive(:write).with(/severity=WARN, pid=2023, foo/)
+        subject.add(Logger::WARN) { :pid => 2023, 'foo' }
       end
     end
 
     context "with a hash" do
       it "should send a message via the logdev" do
         subject.logdev.dev.should_receive(:write).with(/severity=WARN/)
+        subject.logdev.dev.should_receive(:write).with(/pid=2024/)
         subject.logdev.dev.should_receive(:write).with(/foo=bar/)
         subject.logdev.dev.should_receive(:write).with(/man=pants/)
         # The following is equiv to:
         # subject.warn :foo => :bar, :man => :pants
-        subject.add(Logger::WARN) { {:foo => :bar, :man => :pants} }
-        subject.add(Logger::WARN) { {:foo => :bar, :man => :pants} }
-        subject.add(Logger::WARN) { {:foo => :bar, :man => :pants} }
+        subject.add(Logger::WARN) { {:pid => 2025, :foo => :bar, :man => :pants} }
+        subject.add(Logger::WARN) { {:pid => 2026, :foo => :bar, :man => :pants} }
+        subject.add(Logger::WARN) { {:pid => 2027, :foo => :bar, :man => :pants} }
       end
     end
   end
